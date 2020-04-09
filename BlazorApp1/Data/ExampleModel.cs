84db8d8bab7f0207efb8415a642c4c6de2eb83b1
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2020-04-09 18:17:15 +0200

    Add project files.

---
+++ BlazorApp1/Data/ExampleModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
+{
+    public class ExampleModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+}
