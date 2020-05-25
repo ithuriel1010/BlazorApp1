9c522569de6dc82818495fc643549ec5ea0267ec
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2020-05-25 13:58:54 +0200

    Added comparison of respondent's data and average data of all respondents

---
+++ BlazorApp1/Data/GetAllData.cs
@@ -0,0 +1,127 @@
+﻿using EFGetStarted;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class GetAllData
+    {
+        Respondent respondent;
+
+        public GetAllData(Respondent respondent)
+        {
+            this.respondent = respondent;
+        }
+
+        public double GetAverageAge(Respondent respondent)
+        {
+            double avgAge;
+
+            using (var db = new BloggingContext())
+            {
+                avgAge = db.Respondent.Where(x => x.RespondentId != respondent.RespondentId).Average(x => x.Wiek);
+            }
+            return (avgAge);
+        }
+
+        public double GetAverageFiz(Respondent respondent)
+        {
+            double avgFiz;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId != respondent.RespondentId).SelectMany(x => x.StanyFizyczne).Average(x=>x.Poziom);
+                avgFiz = group;
+            }
+            return (avgFiz);
+        }
+
+        public double GetAverageWiedzaMon(Respondent respondent)
+        {
+            double avgWiedzaMon;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId != respondent.RespondentId).SelectMany(x => x.TechnologiaWiedzas).Average(x => x.Poziom);
+                avgWiedzaMon = group;
+            }
+            return (avgWiedzaMon);
+        }
+
+        public double GetAverageOdczuciaMon(Respondent respondent)
+        {
+            double avgOdczuciaMon;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId != respondent.RespondentId).SelectMany(x => x.MonitoringOdczucias).Average(x => x.Poziom);
+                avgOdczuciaMon = group;
+            }
+            return (avgOdczuciaMon);
+        }
+
+        public double GetAverageWiedzaTech(Respondent respondent)
+        {
+            double avgWiedzaTech;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId != respondent.RespondentId).SelectMany(x => x.MonitoringOdczucias).Average(x => x.Poziom);
+                avgWiedzaTech = group;
+            }
+            return (avgWiedzaTech);
+        }
+
+        public double GetResWiedzaTech(Respondent respondent)
+        {
+            double avgWiedzaTech;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId == respondent.RespondentId).SelectMany(x => x.MonitoringOdczucias).Average(x => x.Poziom);
+                avgWiedzaTech = group;
+            }
+            return (avgWiedzaTech);
+        }
+
+        public double GetResFiz(Respondent respondent)
+        {
+            double avgFiz;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId == respondent.RespondentId).SelectMany(x => x.StanyFizyczne).Average(x => x.Poziom);
+                avgFiz = group;
+            }
+            return (avgFiz);
+        }
+
+        public double GetResWiedzaMon(Respondent respondent)
+        {
+            double avgWiedzaMon;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId == respondent.RespondentId).SelectMany(x => x.TechnologiaWiedzas).Average(x => x.Poziom);
+                avgWiedzaMon = group;
+            }
+            return (avgWiedzaMon);
+        }
+
+        public double GetResOdczuciaMon(Respondent respondent)
+        {
+            double avgOdczuciaMon;
+
+            using (var db = new BloggingContext())
+            {
+                var group = db.Respondent.Where(x => x.RespondentId != respondent.RespondentId).SelectMany(x => x.MonitoringOdczucias).Average(x => x.Poziom);
+                avgOdczuciaMon = group;
+            }
+            return (avgOdczuciaMon);
+        }
+
+
+    }
+}
---
+++ BlazorApp1/Data/Walidacje.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
+{
+    public class Walidacje
+    {
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int wyksztalcenie { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int zdrowie { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int pomoc { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int samotnosc { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int bezpieczenstwo { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int technologia { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int internet { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int internet2 { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int monitoring { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int monitoring1 { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int wizyjny { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int czujnik { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int robot { get; set; }
+
+        [Range(0, 1, ErrorMessage = "Wybierz")]
+        public int choraosoba { get; set; }
+
+        [Range(0, 1, ErrorMessage = "Wybierz")]
+        public int mieszkaniezseniorem { get; set; }
+
+        [Range(0, 4, ErrorMessage = "Wybierz")]
+        public int opieka { get; set; }
+
+        [Range(0, 5, ErrorMessage = "Wybierz")]
+        public int mieszkanieZaznacz { get; set; }
+
+        [Required]
+        public bool check { get; set; }
+    }
+}
